626fc529c68a9cbf598f3c52c312ce46775ecc00
Miheenko <karolina.mex@ya.ru>
2014-05-22 11:00:43 +0400

    DZ1

---
+++ project-selenium-tests/src/com/example/tests/MyCreateGroupTests.java
@@ -0,0 +1,31 @@
+
+package com.example.tests;
+
+
+import org.testng.annotations.Test;
+
+public class MyCreateGroupTests extends TestBase {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.groupname = "group name 1";
+    group.header = "header 1";
+    group.footer = "footer 1";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
