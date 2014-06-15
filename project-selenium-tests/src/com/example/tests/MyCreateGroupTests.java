7a125c94bdce88a012a7e887449701d671960e29
Miheenko <karolina.mex@ya.ru>
2014-06-15 23:02:39 +0400

    DZ_5
    List<GroupData>

--- project-selenium-tests/src/com/example/tests/MyCreateGroupTests.java
+++ project-selenium-tests/src/com/example/tests/MyCreateGroupTests.java
@@ -1,6 +1,9 @@
 
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class MyCreateGroupTests extends TestBase {
@@ -8,6 +11,10 @@ public class MyCreateGroupTests extends TestBase {
   public void testNonEmptyGroupCreation() throws Exception {
 	app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
+    
+    //save old state
+    List<GroupData> oldList = app.getGroupHelper().getGroups();
+    //actions
     app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData();
     group.groupname = "group name 1";
@@ -16,15 +23,35 @@ public class MyCreateGroupTests extends TestBase {
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+    //save new state
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+        
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);
   }
   
   @Test
   public void testEmptyGroupCreation() throws Exception {
 	app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
+    
+    //save old state
+    List<GroupData> oldList = app.getGroupHelper().getGroups();
+    
+    //actions
+    GroupData group = new GroupData("", "", "");
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+  //save new state
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+    
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);
   }
 }
