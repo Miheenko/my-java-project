0e53fea3d8e97541728ccd8308688289de996b86
Miheenko <karolina.mex@ya.ru>
2014-06-17 08:41:02 +0400

    Revert "DZ6"
    This reverts commit 31652ffb1b1e108729328d78965c46cd89126872.

--- project-selenium-tests/src/com/example/tests/MyAddAddressTests.java
+++ project-selenium-tests/src/com/example/tests/MyAddAddressTests.java
@@ -1,8 +1,5 @@
 package com.example.tests;
 
-import java.util.Collections;
-import java.util.List;
-import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class MyAddAddressTests extends TestBase {
@@ -10,10 +7,6 @@ public class MyAddAddressTests extends TestBase {
   @Test
   public void testAddressCreation() throws Exception {
 	app.getNavigationHelper().openMainPage();
-	//save old state
-	List<AddressData> oldList = app.getContactHelper().getContacts();
-	
-	//actions
     app.getContactHelper().initNewAddressCreation();
     AddressData address = new AddressData();
     address.firstName = "first_Name";
@@ -32,17 +25,5 @@ public class MyAddAddressTests extends TestBase {
 	app.getContactHelper().fillAddressForm(address);
     app.getContactHelper().submitAddressCreation();
     app.getContactHelper().returnToHomePage();
-  
-  
-  //save new state
-  List<AddressData> newList = app.getContactHelper().getContacts();
-
-  
-  //compare states
-  assertEquals(newList.size(), oldList.size() + 1);
-  
-  oldList.add(address);
-  Collections.sort(oldList);
-  assertEquals(newList, oldList);
   }
 }
--- project-selenium-tests/src/com/example/tests/RemoveAddressTests.java
+++ project-selenium-tests/src/com/example/tests/RemoveAddressTests.java
@@ -1,10 +1,5 @@
 package com.example.tests;
 
-import static org.testng.Assert.assertEquals;
-
-import java.util.Collections;
-import java.util.List;
-
 import org.testng.annotations.Test;
 
 public class RemoveAddressTests extends TestBase {
@@ -12,20 +7,8 @@ public class RemoveAddressTests extends TestBase {
 	@Test
 	public void deleteSomeAddress(){
 		app.getNavigationHelper().openMainPage();
-		//save old state
-		List<AddressData> oldList = app.getContactHelper().getContacts();
-		
-		//actions
 		app.getContactHelper().initRemoveEditContact(3);
 		app.getContactHelper().removeContact();
-		app.getContactHelper().returnToHomePage();   
-		
-		//save new state
-		  List<AddressData> newList = app.getContactHelper().getContacts();
-
-		  
-		  oldList.remove(3);
-		  Collections.sort(oldList);
-		  assertEquals(newList, oldList);
+		app.getContactHelper().returnToHomePage();    
 	}
 }
